168e7731f27e6ec4ce5ae6e3aae4a0ef85daaabb
joao-hideki <joaoohideki12345@gmail.com>
2023-02-20 11:35:54 -0300

    tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,34 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return(
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>11/01/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Contas</td>
+              <td>
+              <PriceHighLight variant="outcome">- R$ 1.000,00</PriceHighLight>
+              </td>
+              <td>Gastos fixos</td>
+              <td>18/01/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
